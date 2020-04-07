067daba6c9adc3805154f6dc1b6c36f3161ea73d
aliaksandr-bandziuk <aliaksandr.bandziuk@gmail.com>
2020-04-07 19:40:56 +0300

    first draft

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+function yourNumber() {
+    let randomNumber = Math.floor(Math.random() * 100) + 1;
+    +prompt('Угадай число от 1 до 100');
+    return yourNumber();
+
+    function answer() {
+        if (yourNumber < randomNumber) {
+            alert('Загаданное число меньше');
+        }
+        if (yourNumber === isNaN) {
+            +prompt('Введи число!');
+        }
+        if (yourNumber === false) {
+            alert('Ты проиграл');
+        }
+    }
+}
