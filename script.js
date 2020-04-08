c7cf4c758b0c9b83c11900761f74a3f513c9e1dd
aliaksandr-bandziuk <aliaksandr.bandziuk@gmail.com>
2020-04-08 13:31:49 +0300

    upd final

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
 'use strict';
 
-function yourNumber() {
+let isNumber = function(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+function startGame() {
     let randomNumber = Math.floor(Math.random() * 100) + 1;
-    +prompt('Угадай число от 1 до 100');
-    return yourNumber();
+    // это выводит загаданное число
+    // alert(randomNumber);
 
     function answer() {
-        if (yourNumber < randomNumber) {
+        let count = prompt('Угадай число от 1 до 100');
+
+        if (count === null) {
+            alert('Игра окончена! Спасибо за игру');
+            return; // это останавливает игру
+        }
+
+        if (!isNumber(count)) {
+            alert('Введите число!');
+            answer();
+        }
+
+        if (+count > randomNumber) {
             alert('Загаданное число меньше');
+            answer();
         }
-        if (yourNumber === isNaN) {
-            +prompt('Введи число!');
+
+        if (+count < randomNumber) {
+            alert('Загаданное число больше');
+            answer();
         }
-        if (yourNumber === false) {
-            alert('Ты проиграл');
+
+        if (+count === randomNumber) {
+            alert('Бинго! Вы восхитительны!');
         }
+       
     }
+    answer();
 }
+
+startGame(); 
